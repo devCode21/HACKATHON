c290f9129da23ce1db4012743b7b5040f55b89e7
kadak <gwkadak@gmail.com>
2025-03-21 16:00:17 +0530

    a

--- webserver/Train_model_using_dataset/emotionrecogination.py
+++ webserver/Train_model_using_dataset/emotionrecogination.py
@@ -2,7 +2,7 @@ import kagglehub
 import os
 import pandas as pd
 
-# Download the dataset (you already have this step)
+
 path = kagglehub.dataset_download("ejlok1/toronto-emotional-speech-set-tess")
 
 
@@ -11,17 +11,16 @@ labels = []
 
 for dirname, _, filenames in os.walk(path):
     for filename in filenames:
-        if filename.endswith(".wav"):  # Process only .wav files
+        if filename.endswith(".wav"): 
             paths.append(os.path.join(dirname, filename))
             label = filename.split('_')[2].split('.')[0]
-            labels.append(label.lower())  # Label is the third part of the filename
+            labels.append(label.lower()) 
 
 print(f'Dataset Loaded with {len(paths)} audio files')
 
-# Create a DataFrame with paths and labels
 df = pd.DataFrame({'speech': paths, 'label': labels})
 
-# Print the first few rows
+
 print(df.head())
 
 # Save the DataFrame to CSV
